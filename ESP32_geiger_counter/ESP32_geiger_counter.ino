db539975dad88757ba519d93ad02009d17b486e4
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2022-02-15 19:08:43 +0100

    Added ESP32 code

---
+++ ESP32_geiger_counter/ESP32_geiger_counter.ino
@@ -0,0 +1,109 @@
+#include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+#include <SPIFFS.h>
+
+#define CPM_ARR_SIZE 60
+#define PERIOD_LOG 1
+
+// Conversion factor - CPM to uSV/h
+#define CONV_FACTOR 0.00812
+
+const char * ssid = "";
+const char * password = "";
+
+const int inputPin = 2; //input pin from VIN on cajoe shield
+volatile unsigned long counts = 0; // Tube events
+int cpm = 0; // CPM       
+unsigned long lastCountTime; // Time measurement
+
+//Queue data structure to keep last CPM_ARR_SIZE measurements
+int cpmArray[CPM_ARR_SIZE];
+int cpmArrUsage = 0;
+
+AsyncWebServer apiServer(8080); //json server
+AsyncWebServer httpServer(80); //html server
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("[ INFO ]\tConnecting to wireless network...");
+  }
+  Serial.println("");
+  Serial.println("[ INFO ]\tWiFi connected");
+  Serial.print("[ INFO ]\tIP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("[ ERR  ]\tAn Error has occurred while mounting SPIFFS");
+  } else {
+    Serial.println("[ INFO ]\tFiles present on local storage:");
+    File root = SPIFFS.open("/");
+    File file = root.openNextFile();
+    while (file) {
+      Serial.print("[ INFO ]\tFILE: ");
+      Serial.println(file.name());
+      file = root.openNextFile();
+    }
+  }
+
+  pinMode(inputPin, INPUT); //input pin of cajoe shield
+  attachInterrupt(digitalPinToInterrupt(inputPin), GetEvent, FALLING); //funcition to be called when a pulse is sensed on pin from cajoe shield
+  Serial.println("\n[  OK  ]\tGeiger counter started");
+
+  apiServer.begin();
+  httpServer.begin();
+
+  httpServer.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request -> send(SPIFFS, "/index.html");
+  });
+  httpServer.on("/favicon.png", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request -> send(SPIFFS, "/favicon.png");
+  });
+  httpServer.on("/main.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request -> send(SPIFFS, "/main.js");
+  });
+
+  apiServer.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request -> send(200, "application/json", "{ \"CPM\": " + String(cpm) + ", \"uSvH\": " + String(cpm * CONV_FACTOR) + "}");
+  });
+
+  Serial.println("[  OK  ]\tServer started...");
+  Serial.println("[ INFO ]\tSoftware ready. navigate to ip address on port 80...");
+}
+
+int value = 0;
+
+void loop() {
+
+  //every second update the queue structure with the counted pulses recived in the last second
+  //then, if not enough space is available in the structure, shift the queue and makes space in the last position
+  if (millis() - lastCountTime > 1000) {
+    int tmp = 0;
+    if (cpmArrUsage < CPM_ARR_SIZE) {
+      cpmArray[cpmArrUsage] = counts;
+      counts = 0;
+      cpmArrUsage++;
+    } else {
+      for (int i = 0; i < CPM_ARR_SIZE - 1; i++) {
+        cpmArray[i] = cpmArray[i + 1];
+      }
+      cpmArray[CPM_ARR_SIZE - 1] = counts;
+      counts = 0;
+    }
+    for (int i = 0; i < cpmArrUsage; i++) {
+      tmp += cpmArray[i];
+    }
+    cpm = tmp;
+
+    lastCountTime = millis();
+
+  }
+
+}
+
+//pulse input handler
+ICACHE_RAM_ATTR void GetEvent() {
+  counts++;
+}
---
+++ ESP32_geiger_counter/data/main.js
@@ -0,0 +1,91 @@
+/*
+  Geiger counter web interface by Marco Santi (github.com/marcoSanti)
+*/
+
+
+var endpoint = window.location.protocol + '//' + window.location.hostname + ":8080";
+var CpmChart=null;
+var itemCounts=0;
+var maxItemInGraph = 30;
+
+const CPMGraphConfig = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+          label: 'Counts',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [],
+        }]
+      },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Counts graph'
+          }
+        }
+      },
+  };
+
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+    if(itemCounts<maxItemInGraph) itemCounts++;
+    else{
+      while(itemCounts >= maxItemInGraph){
+          chart.data.labels.shift();
+          chart.data.datasets.forEach((dataset) => {
+            dataset.data.shift();
+          });
+          itemCounts--;
+      }
+      
+      chart.update();
+    }
+}
+
+
+
+
+function fetchData(){
+    $.get(endpoint).done(function(data){
+        console.log(data);
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        addData(CpmChart, time, data["CPM"]);
+        $("#cpm").html(data["CPM"]);
+        $("#usvh").html(data["uSvH"]);
+    });
+
+}
+
+function updateNumberItemsLabel(){
+  $("#lblMaxItemInGraph").html(maxItemInGraph);
+}
+
+
+$(document).ready(function(){
+    setInterval(fetchData, 1000);
+    CpmChart = new Chart(document.getElementById('cpmCountsGraph'),CPMGraphConfig);
+    updateNumberItemsLabel();
+
+    $("#increaseGraphItems").click(function(){
+      maxItemInGraph++;
+      updateNumberItemsLabel();
+    });
+    
+    $("#decreaseGraphItems").click(function(){
+      maxItemInGraph--;
+      updateNumberItemsLabel();
+    });
+})
